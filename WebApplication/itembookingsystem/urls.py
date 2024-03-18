aa62dd3821766d7a0c315a4d32398d2fe6200adb
Toby King <Tobusjo@gmail.com>
2024-03-18 18:06:56 +0000

    Add templates and get home rendering

--- WebApplication/itembookingsystem/urls.py
+++ WebApplication/itembookingsystem/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from helloworld import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #this is a 
+    path('home/', views.home)
 ]
