ba09337f613a6e0f26d5f965072453e5c10dadaa
MatthewObeh08 <Matthewobeh08@gmail.com>
2024-03-28 17:32:12 +0000

    fix render and fix max length on forms and added login in to views

--- WebApplication/helloworld/forms.py
+++ WebApplication/helloworld/forms.py
@@ -2,5 +2,5 @@ from django import forms
 
 
 class loginForm(forms.Form):
-    userName = forms.CharField(label="Username", max_length=100)
-    password = forms.CharField(label="Password", max_length=100)
+    userName = forms.CharField(label="Username", max_length=50)
+    password = forms.CharField(label="Password", max_length=50)
