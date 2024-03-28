68d95fb060aef12f339a08a4dd1db143899ee84c
MatthewObeh08 <Matthewobeh08@gmail.com>
2024-03-28 18:03:03 +0000

    backend function for login

--- WebApplication/helloworld/views.py
+++ WebApplication/helloworld/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from .forms import loginForm
 # Create your views here.
@@ -17,15 +18,25 @@ def bookingPage(request):
 def login(request):
     # if this is a POST request we need to process the form data
     if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
         # create a form instance and populate it with data from the request:
         form = loginForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            
+        
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+        # Redirect to a success page.
+                return render(request,"login.html", {"status": "Success"})
+            else:
+        # Return an 'invalid login' error message.
+        
             # process the data in form.cleaned_data as required
             # ...
             # redirect to a new URL:
-            return render(request,"login.html", {"status": "Success"})
+                return render(request,"login.html", {"status": "Failed"})
 
     # if a GET (or any other method) we'll create a blank form
     else:
