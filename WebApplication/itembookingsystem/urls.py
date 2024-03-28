26b5c2baf0fb8cfa45bb57ba0dcc29e83bad30ca
MatthewObeh08 <Matthewobeh08@gmail.com>
2024-03-28 18:03:03 +0000

    backend function for login

--- WebApplication/itembookingsystem/urls.py
+++ WebApplication/itembookingsystem/urls.py
@@ -13,6 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path
 
@@ -22,5 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('home/', views.home),
-    path('login/',views.login)
+    path('login/',views.login),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+
 ]
