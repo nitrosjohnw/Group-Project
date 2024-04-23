c88ac9931e1766e8bfdc243412c5bf77f91fa47e
Toby King <66286167+TobyDHKing@users.noreply.github.com>
2024-04-23 18:48:01 +0100

    Fix: Import all required modules in views

--- WebApplication/helloworld/views.py
+++ WebApplication/helloworld/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from helloworld.forms import loginForm
 # Create your views here.
 def home(request):
 
--- WebApplication/itembookingsystem/urls.py
+++ WebApplication/itembookingsystem/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
 from django.urls import path
-
 from helloworld import views
 
 urlpatterns = [
