545af94e53c52677421814ab7dea6e492bed0a67
Toby King <66286167+TobyDHKing@users.noreply.github.com>
2024-04-25 17:27:58 +0100

    fix: Loading Module

--- WebApplication/helloworld/admin.py
+++ WebApplication/helloworld/admin.py
@@ -5,4 +5,8 @@ from django.contrib import admin
 
 from .models import User_Account, Admin_Account, Booking, Equipment
 
-admin.site.register(User_Account,Admin_Account,Booking,Equipment)
+admin.site.register(User_Account)
+admin.site.register(Admin_Account)
+admin.site.register(Booking)
+admin.site.register(Equipment)
+
