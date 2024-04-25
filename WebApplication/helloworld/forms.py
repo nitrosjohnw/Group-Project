439c2c259e83d32925ea3240475b62a731401c95
Toby King <66286167+TobyDHKing@users.noreply.github.com>
2024-04-25 15:56:10 +0100

    Feat: Add forms for booking

--- WebApplication/helloworld/forms.py
+++ WebApplication/helloworld/forms.py
@@ -1,9 +1,20 @@
 from django import forms
 from django.contrib.auth.models import User
 
-
 class loginForm(forms.Form):
     username = forms.CharField(label="Username", max_length=50)
     password = forms.CharField(label="Password", max_length=50)
     class Meta:
         model = User
+
+
+
+class bookingForm(forms.Form):
+    accountID = forms.CharField(label = "accountID",max_length = 30)
+    itemID = forms.CharField(label = "ID",max_length = 30)
+    startDate = forms.CharField(label = "Start Date",max_length = 30)
+    endDate = forms.CharField(label = "End Date",max_length = 30)
+    bookingStatus = forms.CharField(label = "Status",max_length = 30) # this will just be true, admin will not have to accept these
+    class Meta:
+        fields = ['accountID','itemID','startDate','endDate','bookingStatus']
+        
