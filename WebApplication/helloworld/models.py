9dea2e518306bdba94a4fd1342970598be7c2d21
Toby King <66286167+TobyDHKing@users.noreply.github.com>
2024-04-26 01:23:49 +0100

    fix: Equipment Model

--- WebApplication/helloworld/models.py
+++ WebApplication/helloworld/models.py
@@ -58,22 +58,34 @@ class Booking(models.Model):
         return booking
     
 class Equipment(models.Model):
+    LOCATIONS = [
+        ("XRLBC", "XRL Lab Blue Cabinet"),
+        ("XRLBCL", "XRL Lab Blue Cabinet Large"),
+        ("XRL", "XRL Lab"),
+        ("OTHER", "Other"),
+        ('XRLMWC', "XRLab Medium Wooden Cabinet")
+    ]
+    STATUS = [
+        ("A", "Available"),
+        ("ON", "On Loan"),
+        ("D", "Decommissioned"),
+        ("R", "Repairing"),
+    ]
+
     # Equipment Details
     equipmentName = models.CharField(max_length = 30)
     equipmentType = models.CharField(max_length = 30)
-    equipmentAssests = models.CharField(max_length = 30)
-    equipmentStatus = models.CharField(max_length = 30)
-    equipmentWarranty = models.CharField(max_length = 30)
-    equipmentOnSite = models.BooleanField()
-    equipmentAudit =  models.CharField(max_length = 30)
-    
+    equipmentQuantity = models.IntegerField( default= 1)
+    equipmentLocation = models.CharField(max_length = 30, choices = LOCATIONS , default = "OTHER")
+    equipmentAudit =  models.DateField()
+    equipmentStatus = models.CharField(max_length = 20, choices = STATUS, default = "A")
+
     equipmentID = models.CharField(max_length = 30, primary_key = True)
     
     def __str__(self):
         return self.equipmentID  
     
     @classmethod
-    def create(cls, equipmentName, equipmentType, equipmentAssests, equipmentStatus, equipmentWarranty, equipmentOnSite, equipmentAudit, equipmentID):
-        equipment = cls(equipmentName=equipmentName, equipmentType=equipmentType, equipmentAssests = equipmentAssests, equipmentStatus = equipmentStatus, equipmentWarranty = equipmentWarranty, equipmentOnSite = equipmentOnSite, equipmentAudit = equipmentAudit, equipmentID = equipmentID)
+    def create(cls, equipmentName, equipmentType, equipmentQuantity, equipmentLocation, equipmentAudit, equipmentStatus, equipmentID):
+        equipment = cls(equipmentName=equipmentName, equipmentType=equipmentType, equipmentQuantity = equipmentQuantity, equipmentLocation = equipmentLocation, equipmentAudit = equipmentAudit, equipmentStatus = equipmentStatus, equipmentID = equipmentID)
         return equipment
-    
