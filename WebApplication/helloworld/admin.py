8485f192ef931b32995dc8ecc2fc4cf56beed76e
Davidcreig <davidlaptop@outlook.es>
2024-05-01 02:34:25 +0200

    Reports now print on separate pages

--- WebApplication/helloworld/admin.py
+++ WebApplication/helloworld/admin.py
@@ -3,41 +3,49 @@ from helloworld.models import Equipment
 from django.http import HttpResponse
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import Table, TableStyle
+from reportlab.platypus import SimpleDocTemplate ,Table, TableStyle, PageBreak
 from reportlab.lib import  colors
+from reportlab.lib.styles import  getSampleStyleSheet
 
 #reports method
-def download_pdf(self,request, queryset):
+def download_pdf(self, request, queryset):
     model_name = self.model.__name__
-    response = HttpResponse(content_type = 'application/pdf')
-    response['Content-Disposition'] = f'attachment; filename = {model_name}.pdf'
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename={model_name}.pdf'
 
-    pdf = canvas.Canvas(response,pagesize = letter)
-    pdf.setTitle('PDF Report')
-    
-    #creating table
+    pdf = SimpleDocTemplate(response, pagesize=letter)
+    elements = []
+
+    # Get the sample style sheet for setting table styles
+    styles = getSampleStyleSheet()
+
+    # Create table header
     headers = [field.verbose_name for field in self.model._meta.fields]
     data = [headers]
 
+    # Add data rows
     for obj in queryset:
-        data_row = [str (getattr(obj,field.name)) for field in self.model._meta.fields]
+        data_row = [str(getattr(obj, field.name)) for field in self.model._meta.fields]
         data.append(data_row)
 
-    table = Table(data)
-    table.setStyle(TableStyle(
-        [
-        ('BACKGROUND', (0,0), (-1,0), colors.grey),
-        ('GRID', (0,0), (-1,-1), 1, colors.black),
-        ]
-    ))
+    # Calculate how many rows can fit on a single page
+    rows_per_page = 50  # Adjust this value based on your content and page size
 
-    canvas_width = 600
-    canvas_height = 600
+    # Split data into chunks for multiple pages
+    for i in range(0, len(data), rows_per_page):
+        chunk = data[i:i + rows_per_page]
 
-    table.wrapOn(pdf,canvas_width,canvas_height)
-    table.drawOn(pdf,10,canvas_height - len(data))
-    
-    pdf.save()
+        # Create table for each chunk
+        table = Table(chunk)
+        table.setStyle(TableStyle([
+            ('BACKGROUND', (1, 1), (1, -1), colors.grey),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black),
+        ]))
+
+        elements.append(table)
+        elements.append(PageBreak())
+
+    pdf.build(elements)
     return response
 download_pdf.short_description = "Download selected items as PDF"
 # Register your models here.
